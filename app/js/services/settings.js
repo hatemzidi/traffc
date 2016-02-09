4da86dae08cced47b544d241be3613a0716ceb6f
hatem.zidi <hatem.zidi@gmail.com>
2016-02-09 16:03:06 +0100

    [DEV] extract utils methods from settings

--- app/js/services/settings.js
+++ app/js/services/settings.js
@@ -13,16 +13,6 @@ angular.module('traffc')
             get: function () {
                 this.data = $storage.get('_traffc_settings');
                 return this.data;
-            },
-            //todo extract to $utils provider
-            isMobile: function () {
-                return $.browser.mobile;
-            },
-            isEvening: function () {
-                var now = new Date();
-
-                // night mode is available from 18:00 to 06:00
-                return (6 >= now.getHours() || now.getHours() >= 18);
             }
         };
     }]);
